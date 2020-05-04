a051f10e417658db0c603aa64ed4092a0458ce81
wesley guerra <wesleyguerra9@gmail.com>
2020-05-04 10:41:57 -0300

    atualização

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+
+
+
+$(function () {
+    $('.links a').click(function () {
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html,body').animate({ 'scrollTop': offSetTop });
+
+        return false;
+    })
+
+
+
+    $('.button a').click(function () {
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+        $('html,body').animate({ 'scrollTop': offSetTop });
+
+        return false;
+    })
+})
+
+
+
+/* funcionalidade no menu */
+const buttonMenu = document.querySelector('.menu-opener')
+buttonMenu.addEventListener('click',()=>{
+    document.querySelector('.links').classList.toggle('oculte')
+})
+
+
+
 // /* animação */
 const target = document.querySelectorAll('[data-anime]')
 const animationClass =  'animate';
@@ -19,3 +53,20 @@ if(target.length){
         animeScroll();
     })
 };
+
+
+/* botão iframe */
+const btn = document.querySelector('.btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+btn.addEventListener("click", function () {
+    modalOverlay.classList.remove('hide');
+    modalOverlay.querySelector("iframe").src =`https://www.youtube.com/embed/n161-SxGDPQ`; 
+
+
+});
+
+document.querySelector('.close-modal').addEventListener("click", function () {
+    modalOverlay.classList.add("hide");
+    modalOverlay.querySelector("iframe").src = ``;
+})
+
